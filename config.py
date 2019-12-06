df3632b5360aae29be10bdc12dd4828bc5552a22
awkz <bararamadhan@gmail.com>
2019-12-06 20:50:36 +0700

    db uri

--- config.py
+++ config.py
@@ -103,7 +103,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI','mysql+pymysql://admin:12345678@localhost/sdbayesdev?charset=utf8mb4')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'mysql+pymysql://admin:12345678@localhost/sdbayesdev?charset=utf8mb4')
     SSL_DISABLE = (os.environ.get('SSL_DISABLE', 'True') == 'True')
 
     @classmethod
